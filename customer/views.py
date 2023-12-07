5698c78131ee8fb91640de3ea332021b04d73387
Kusuma Sushmitha <sk10689@nyu.edu>
2023-12-07 16:47:55 -0500

    write anything

--- customer/views.py
+++ customer/views.py
@@ -1,4 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from .forms import SignUpForm
+from django.contrib import messages
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.email = form.cleaned_data.get('email')
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+            return redirect('customer:home_page')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def custom_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return redirect('customer:home_page')
+            else:
+                messages.error(request, "Your account is inactive.")
+        else:
+            messages.error(request, "Invalid username or password.")
+
+    return render(request, 'login.html')
+
+def custom_logout(request):
+    logout(request)
+    messages.info(request, "You have successfully logged out.")
+    return redirect('customer:login')
 
 
 def get_home_page(request):
