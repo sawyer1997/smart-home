fd15c0fb83274dab0e992eee4e6e0d8ea08f948d
Kusuma Sushmitha <sk10689@nyu.edu>
2023-12-12 16:22:23 -0500

    added service location

--- customer/urls.py
+++ customer/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
-    path('profile/', views.view_profile, name='view_profile')
+    path('profile/', views.view_profile, name='view_profile'),
+    path('service_locations/', views.service_locations, name='service_locations'),
+    path('add_service_location/', views.add_service_location, name='add_service_location'),
+    path('delete_service_location/<int:location_id>/', views.delete_service_location, name='delete_service_location'),
 ]
