d86ea88aaefdb79cec5820e74c289d6064d4601d
Kusuma Sushmitha <sk10689@nyu.edu>
2023-12-12 16:22:23 -0500

    added service location

--- customer/forms.py
+++ customer/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import ServiceLocation
 
 
 class SignUpForm(UserCreationForm):
@@ -15,4 +16,14 @@ class SignUpForm(UserCreationForm):
             'last_name': 'Last Name',
         }
         fields = ('first_name', 'last_name', 'email', 'username', 'password1', 'password2')
-        
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class ServiceLocationForm(forms.ModelForm):
+    class Meta:
+        model = ServiceLocation
+        fields = ['apt_unit', 'street', 'state', 'zipcode', 'apt_name', 'move_in_date', 'area', 'bedroom', 'occupants']
+        widgets = {
+            'move_in_date': DateInput(),
+        }
--- customer/views.py
+++ customer/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate, logout
-from .forms import SignUpForm
+from .forms import SignUpForm, ServiceLocationForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from .models import ServiceLocation
 
 def signup(request):
     if request.method == 'POST':
@@ -59,3 +59,33 @@ def view_profile(request):
         'email': request.user.email
     }
     return render(request, 'profile_view.html', context)
+
+def service_locations(request):
+    locations = ServiceLocation.objects.filter(
+        user=request.user,
+        is_active=True,
+        )
+    return render(request, 'service_locations.html', {'locations': locations})
+
+@login_required
+def add_service_location(request):
+    if request.method == 'POST':
+        form = ServiceLocationForm(request.POST)
+        if form.is_valid():
+            service_location = form.save(commit=False)
+            service_location.user = request.user
+            service_location.save()
+            return redirect('customer:service_locations')
+    else:
+        form = ServiceLocationForm()
+    return render(request, 'add_service_location.html', {'form': form})
+
+@login_required
+def delete_service_location(request, location_id):
+    location = get_object_or_404(ServiceLocation, pk=location_id, user=request.user)
+    if request.method == 'POST':
+        location.is_active = False
+        location.save()
+        messages.success(request, 'Service location is deleted.')
+        return redirect('customer:service_locations')
+    return render(request, 'delete_service_location.html', {'location': location})
